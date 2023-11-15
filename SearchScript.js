5b485e7830a89b7c0e1dfb89906dc55712482390
Eric Jungbluth <ericmellow@gmail.com>
2023-11-15 15:17:34 -0700

    Update SearchScript.js

--- SearchScript.js
+++ SearchScript.js
@@ -1,6 +1,36 @@
+init()
+
+import * as ComposerHandler from './utils.js'
+
 var cancelButton = document.querySelector('.cancel-btn');
 var saveButton = document.querySelector('.save-btn');
 var messageInput = document.querySelector('.search-input');
+
+function cancel() {
+  ComposerHandler.sendMessage('cancel');
+}
+
+function init() {
+  ComposerHandler.sendMessage('ready', {
+      height: document.documentElement.scrollHeight,
+    });
+}
+
+function save() {
+  const ansStarter = ComposerHandler.getStarterPowerUpANS();
+  const headline = messageInput.value
+  const ansCustomEmbed = {
+      ...ansStarter,
+      config: {
+        headline,
+      },
+    };
+
+    ComposerHandler.sendMessage('data', ansCustomEmbed);
+}
+
+saveButton.addEventListener('click', save());
+cancelButton.addEventListener('click', cancel());
 
 //Enters range and generates random number
 rangeButton.addEventListener('click', function() {
@@ -18,63 +48,3 @@ maxNum.addEventListener('keyup', function() {
   };
 });
 
-//Function to generate random number
-function GenerateRandomNumber() {
-  maxNum = parseInt(maxNum.value);
-  minNum = parseInt(minNum.value);
-  var number = Math.floor(Math.random() * (maxNum - minNum + 1))  + minNum;
-  console.log(number);
-  return number;
-};
-
-//Main Buttons disable
-userGuess.addEventListener('input', function() {
-  if (userGuess.value.length) {
-    clearButton.classList.add('button-enabled');
-    guessButton.classList.add('button-enabled');
-    resetButton.classList.add('button-enabled');
-  } else {
-    clearButton.classList.remove('button-enabled');
-    guessButton.classList.remove('button-enabled');
-    resetButton.classList.remove('button-enabled');
-  };
-})
-
-//Guess Button submit and appropriate response
-guessButton.addEventListener('click', function () {
-  var newGuess = userGuess.value;
-// console.log(randomNumber);
-newGuess = parseInt(newGuess, 10);
-lastGuess.innerText = newGuess;
-if (Number.isNaN(newGuess)) { 
-  lastGuess.innerText = 'Oops!';
-  hint.innerText = 'Please guess a number between ' + minNum + ' and ' + maxNum +'.';
-}  else if (newGuess > maxNum) {
-  lastGuess.innerText = 'Oops!';
-  hint.innerText = 'Please guess a number between ' + minNum + ' and ' + maxNum +'.';
-} else if (newGuess < minNum) {
-  lastGuess.innerText = 'Oops!';
-  hint.innerText = 'Please guess a number between ' + minNum + ' and ' + maxNum +'.';
-} else if (newGuess === randomNumber) {
-  hint.innerText = 'BOOM!'; 
-} else if (randomNumber < newGuess) {
-  hint.innerText = 'That is too high'; 
-} else {
-  hint.innerText = 'That is too low';
-}
-});
-
-//Clear Button function
-clearButton.addEventListener('click', function () {
-  userGuess.value = "";
-  clearButton.classList.remove('button-enabled');
-  guessButton.classList.remove('button-enabled');
-});
-
-//Reset Button function
-resetButton.addEventListener('click', function() {
-  location.reload();
-  clearButton.classList.remove('button-enabled');
-  guessButton.classList.remove('button-enabled');
-  resetButton.classList.remove('button-enabled');
-});
