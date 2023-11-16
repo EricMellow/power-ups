256b3eb8eb95b62f451ce80de2c7db8798428e47
Eric Jungbluth <ericmellow@gmail.com>
2023-11-16 08:51:12 -0700

    Update SearchScript.js

--- SearchScript.js
+++ SearchScript.js
@@ -5,15 +5,9 @@ var messageInput = document.querySelector('.search-input');
 saveButton.addEventListener('click', function() {save()});
 cancelButton.addEventListener('click', function() {cancel()});
 
-messageInput.addEventListener('input', function() {
-  console.log('hit', messageInput.value)
-})
-
 window.addEventListener("load", (event) => {
-  console.log("page is fully loaded");
   init()
 });
-
 
 function parseQueryString(key) {
   if (typeof window === 'undefined') {
@@ -51,12 +45,10 @@ function getStarterPowerUpANS (idPrefix = 'custom_powerup') {
 };
 
 function cancel() {
-  console.log('SEARCH CANCEL')
   sendMessage('cancel');
 }
 
 function init() {
-  console.log('INIT!!!!!!')
   sendMessage('ready', {
       height: document.documentElement.scrollHeight,
     });
@@ -65,8 +57,6 @@ function init() {
 function save() {
   const ansStarter = getStarterPowerUpANS();
   const headline = messageInput.value
-  console.log(ansStarter)
-  console.log('HEADLINE', headline)
   const ansCustomEmbed = {
       ...ansStarter,
       config: {
