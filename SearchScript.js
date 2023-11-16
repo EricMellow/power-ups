f1dc26a78b0db629a7b6cec4480adc034110aceb
Eric Jungbluth <ericmellow@gmail.com>
2023-11-16 07:54:51 -0700

    Update SearchScript.js

--- SearchScript.js
+++ SearchScript.js
@@ -4,19 +4,30 @@ var cancelButton = document.querySelector('.cancel-btn');
 var saveButton = document.querySelector('.save-btn');
 var messageInput = document.querySelector('.search-input');
 
-function cancel(){
+saveButton.addEventListener('click', function() {save()});
+cancelButton.addEventListener('click', function() {cancel()});
+
+messageInput.addEventListener('input', function() {
+  console.log('hit', messageInput.value)
+})
+
+window.onload = (event) => {
+  init()
+};
+
+function cancel() {
   console.log('SEARCH CANCEL')
   ComposerHandler.sendMessage('cancel');
 }
 
-function init(){
+function init() {
   console.log('INIT!!!!!!')
   ComposerHandler.sendMessage('ready', {
       height: document.documentElement.scrollHeight,
     });
 }
 
-function save(){
+function save() {
   const ansStarter = ComposerHandler.getStarterPowerUpANS();
   const headline = messageInput.value
   console.log(ansStarter)
@@ -31,11 +42,4 @@ function save(){
     ComposerHandler.sendMessage('data', ansCustomEmbed);
 }
 
-saveButton.addEventListener('click', function() {save()});
-cancelButton.addEventListener('click', function() {cancel()});
 
-messageInput.addEventListener('input', function() {
-  console.log('hit', messageInput.value)
-})
-
-init()
