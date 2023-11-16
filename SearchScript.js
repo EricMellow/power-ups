afbd3101cccfeaa33605cc5b21c1bcdfea52e604
Eric Jungbluth <ericmellow@gmail.com>
2023-11-16 08:02:45 -0700

    Update SearchScript.js

--- SearchScript.js
+++ SearchScript.js
@@ -1,5 +1,3 @@
-import * as ComposerHandler from './utils.js'
-
 var cancelButton = document.querySelector('.cancel-btn');
 var saveButton = document.querySelector('.save-btn');
 var messageInput = document.querySelector('.search-input');
@@ -15,20 +13,55 @@ window.onload = (event) => {
   init()
 };
 
+function parseQueryString(key) {
+  if (typeof window === 'undefined') {
+    return null;
+  }
+
+  const url = new URL(window.location.href);
+  return url.searchParams.get(key);
+};
+
+function getPayload () {return JSON.parse(parseQueryString('p'))};
+
+function sendMessage(action, data = {}) {
+  if (typeof window !== 'undefined') {
+    const messagePayload = {
+      source: 'custom_embed',
+      action,
+      data,
+      key: parseQueryString('k'),
+    };
+    if (action === 'ready') {
+      messagePayload.isAnsRequired = true;
+    }
+    window.parent.postMessage(JSON.stringify(messagePayload), '*');
+  }
+};
+
+function getStarterPowerUpANS (idPrefix = 'custom_powerup') {
+  const dateNow = Date.now();
+  return {
+    id: `${idPrefix}_${dateNow}`,
+    url: '/',
+    config: {},
+  };
+};
+
 function cancel() {
   console.log('SEARCH CANCEL')
-  ComposerHandler.sendMessage('cancel');
+  sendMessage('cancel');
 }
 
 function init() {
   console.log('INIT!!!!!!')
-  ComposerHandler.sendMessage('ready', {
+  sendMessage('ready', {
       height: document.documentElement.scrollHeight,
     });
 }
 
 function save() {
-  const ansStarter = ComposerHandler.getStarterPowerUpANS();
+  const ansStarter = getStarterPowerUpANS();
   const headline = messageInput.value
   console.log(ansStarter)
   console.log('HEADLINE', headline)
@@ -39,7 +72,7 @@ function save() {
       },
     };
 
-    ComposerHandler.sendMessage('data', ansCustomEmbed);
+    sendMessage('data', ansCustomEmbed);
 }
 
 
